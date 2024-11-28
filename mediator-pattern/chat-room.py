2ccb3b3c1b16a00df37e9296b84acd9d2d32077e
Shreyash <hello@webdevcaptain.com>
2024-11-28 13:54:42 +0000

    Add mediator pattern and it's python implementation

---
+++ mediator-pattern/chat-room.py
@@ -0,0 +1,27 @@
+# Mediator
+class ChatRoom:
+    def show_message(self, user, msg):
+        # Mediator handles displaying of messages
+        print(f"[{user}]: {msg}")
+
+
+# Colleague
+class User:
+    def __init__(self, name, chatroom):
+        self.name = name
+        self.chatroom = chatroom  # User is associated with a chatroom mediator
+
+    def send_message(self, message):
+        # User sends message via chatroom mediator
+        self.chatroom.show_message(self.name, message)
+
+
+# Dry run
+if __name__ == "__main__":
+    chatroom = ChatRoom()
+
+    user1 = User("Ayush", chatroom)
+    user2 = User("Shreyash", chatroom)
+
+    user1.send_message("Hello, Shreyash")
+    user2.send_message("Hey Ayush")
---
+++ mediator-pattern/concept.md
@@ -0,0 +1,3 @@
+The *Mediator Design Pattern* is used to reduce the complexity of communication between multiple objects or classes. Instead of having objects refer to and call each other directly, they use a `mediator` to `manage interactions`.
+
+* Example 1: Chatroom mediator
